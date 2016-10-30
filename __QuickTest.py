ba9cac470c3c66f91896c9fcd3bb84bc898d215d
Tharaka Devinda <tdevinda@gmail.com>
2016-10-31 00:26:34 +0530

    bug fix for div by zero and date issue

--- __QuickTest.py
+++ __QuickTest.py
@@ -2,3 +2,5 @@ from adaptors.user.github import GithubAdaptor
 
 x = GithubAdaptor()
 print(x.rate('agentmilindu'))
+print(x.rate('tdevinda'))
+print(x.rate('THarsh'))
--- adaptors/user/github.py
+++ adaptors/user/github.py
@@ -1,5 +1,5 @@
 import math
-import datetime
+from datetime import datetime
 
 from adaptors.Adaptor import Adaptor
 import re
@@ -17,7 +17,7 @@ class GithubAdaptor(Adaptor):
         print(answer)
         r = self.get('https://api.github.com/users/'+answer)
         j = r.json()
-        print(j)
+        #print(j)
         return self.get_score(j['public_repos'], j['created_at'], j['updated_at'], j['followers'], j['following'])
 
 
@@ -30,9 +30,12 @@ class GithubAdaptor(Adaptor):
 
     def get_score(self, repos, joined_date, updated_date, followers, following):
         repo_score = 50 * ( 1 - 1 / (repos + 1))
-        active_period = math.ceil((datetime.strptime(updated_date, "%Y-%m-%dT%H:%M:%SZ") - datetime.strptime(joined_date, "%Y-%m-%dT%H:%M:%SZ")) / 7)
+        date_diff = datetime.strptime(updated_date, "%Y-%m-%dT%H:%M:%SZ") - datetime.strptime(joined_date, "%Y-%m-%dT%H:%M:%SZ")
+
+        active_period = math.ceil(abs(date_diff).days / 30)
         date_score = 30 * (1 - (1 / active_period))
-        follower_score = 10 * (1 - (1 / followers))
-        following_score = 10 * (1 - (1 / following))
+        follower_score = 10 * (1 - (1 / (followers + 1)))
+        following_score = 10 * (1 - (1 / (following + 1)))
+        print(repos,repo_score,active_period, date_score,followers,follower_score, following,following_score)
         return repo_score + date_score + follower_score + following_score
 

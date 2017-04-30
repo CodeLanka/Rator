c049e63e35e59e067fb20215b05c65e4d3b535ba
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2017-04-30 10:09:52 +0530

    Updated the Profile adaptor, added Twitter and IdeaMart adaptors

---
+++ adaptors/user/ideamart.py
@@ -0,0 +1,14 @@
+from adaptors.Adaptor import Adaptor
+
+
+
+class IdeamartAdaptor(Adaptor):
+
+    def __init__(self):
+        pass
+
+    def id(self, answer):
+        return 0
+
+    def username(self, answer):
+        return 0
---
+++ adaptors/user/twitter.py
@@ -0,0 +1,14 @@
+from adaptors.Adaptor import Adaptor
+
+
+
+class TwitterAdaptor(Adaptor):
+
+    def __init__(self):
+        pass
+
+    def url(self, answer):
+        return 0
+
+    def handle(self, answer):
+        return 0
